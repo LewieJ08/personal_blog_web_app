a6619a040ef29f5368b8ca5783bcb6cd379e20cb
LewieJ08 <lewiej08@gmail.com>
2025-04-01 11:49:38 +0100

    setup backend

--- personal_blog_web_app/app.py
+++ personal_blog_web_app/app.py
@@ -1,11 +1,34 @@
 from flask import Flask, render_template
+import os
+import json 
+import datetime
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET","POST"])
 
 def index():
-    render_template("index.html")
+    return render_template("index.html")
+
+@app.route("/article", methods=["GET","POST"])
+
+def article():
+    return render_template("article.html")
+
+@app.route("/admin", methods=["GET","POST"])
+
+def admin():
+    return render_template("admin.html")
+
+@app.route("/edit", methods=["GET","POST"])
+
+def edit():
+    return render_template("edit.html")
+
+@app.route("/new", methods=["GET","POST"])
+
+def new():
+    return render_template("new.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
