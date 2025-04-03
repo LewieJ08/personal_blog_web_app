4b2a4e5914839e1f4736268fc9f5b5b26b966e4d
LewieJ08 <lewiej08@gmail.com>
2025-04-03 16:08:49 +0100

    JSON integration

--- personal_blog_web_app/app.py
+++ personal_blog_web_app/app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request
 import os
 import json 
-import datetime
 
 app = Flask(__name__)
+
+JSONFILE = "articles.json"
 
 @app.route("/", methods=["GET","POST"])
 
@@ -28,7 +29,46 @@ def edit():
 @app.route("/new", methods=["GET","POST"])
 
 def new():
-    return render_template("new.html")
+    with open(JSONFILE, "r") as file:
+        data = json.load(file)
+
+    try:
+        if request.method == "POST":
+            title = request.form["title"]
+            date = request.form["date"]
+            content = request.form["content"]
+
+            iD = 1
+            current_ids = []
+
+            for item in data:
+                current_ids.append(item["id"])
+
+            while iD in current_ids:
+                iD += 1
+
+            data.append({
+                    "id": iD,
+                    "title": title,
+                    "date": date,
+                    "content": content
+                })
+            
+            with open(JSONFILE, "w") as file:
+                json.dump(data,file,indent=4)
+
+            return render_template("new.html", title = title, date = date)
+        
+    except ValueError:
+        print("Form field empty")
+        return render_template("new.html",title = None)
+    
+    return render_template("new.html",title = None)
 
 if __name__ == "__main__":
+
+    if not os.path.exists(JSONFILE):
+        with open(JSONFILE, "w") as file:
+            json.dump([],file)
+
     app.run(debug=True)
