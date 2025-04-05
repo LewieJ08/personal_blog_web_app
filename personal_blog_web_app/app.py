c4a3890f6e7578c43f774fcd5b1745d11709f243
LewieJ08 <lewiej08@gmail.com>
2025-04-05 19:54:02 +0100

    http auth

--- personal_blog_web_app/app.py
+++ personal_blog_web_app/app.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
+from functools import wraps
 import os
 import json 
 
 app = Flask(__name__)
 
 JSONFILE = "articles.json"
+
+def auth_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if auth and auth.username == "admin" and auth.password == "password":
+            return f(*args, **kwargs)
+        
+        return make_response("Could not verify your login.", 401, {"WWW-Authenticate": "Basic realm='Login Required'"})
+    
+    return decorated
 
 @app.route("/", methods=["GET","POST"])
 
@@ -16,17 +28,22 @@ def index():
 def article():
     return render_template("article.html")
 
+# Admin pages
+
 @app.route("/admin", methods=["GET","POST"])
+@auth_required
 
 def admin():
     return render_template("admin.html")
 
 @app.route("/edit", methods=["GET","POST"])
+@auth_required
 
 def edit():
     return render_template("edit.html")
 
 @app.route("/new", methods=["GET","POST"])
+@auth_required
 
 def new():
     with open(JSONFILE, "r") as file:
@@ -37,6 +54,10 @@ def new():
             title = request.form["title"]
             date = request.form["date"]
             content = request.form["content"]
+
+            if not title or not date or not content:
+                print("Form field empty")
+                return render_template("new.html", title = None)
 
             iD = 1
             current_ids = []
